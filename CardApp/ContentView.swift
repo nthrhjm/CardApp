b5112068d74e56f57030879767f96fc23e754ce5
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-06 15:22:30 +0900

    Initial Commit

---
+++ CardApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardApp
+//
+//  Created by nhajime on 2020/07/06.
+//  Copyright © 2020 nhajime. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

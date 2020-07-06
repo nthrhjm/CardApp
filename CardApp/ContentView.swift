8ee75e8a3fec34e1369f55820b6f425aac47650c
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-06 17:13:37 +0900

    add card.png to assets

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cards = ["かばん": "bag",
+                 "自動車": "car",
+                 "誕生日": "birthday",]
+    @State var japanese = "かばん"
+    @State var isJapanese = true
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text(isJapanese ? japanese : cards[japanese]!)
+            HStack {
+                Button(action:{ self.isJapanese.toggle()}) {
+                    Text("裏返す")
+                }
+                Button(action: {
+                    self.isJapanese = true
+//                    self.japanese = self.cards.randomElement()!.key
+                    if let elemet = self.cards.randomElement() {
+                        self.japanese = elemet.key
+                    }
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
     }
 }
 
